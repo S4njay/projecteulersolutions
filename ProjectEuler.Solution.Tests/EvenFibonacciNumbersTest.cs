f1e36eaafe4acf2d3e773fc134ce4acc51ced7c5
sanjay <sanjay.au@gmail.com>
2016-10-09 03:04:57 +1100

    Problem2: Even Fibonacci numbers

---
+++ ProjectEuler.Solution.Tests/EvenFibonacciNumbersTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ProjectEuler.Solutions;
+
+namespace ProjectEuler.Solution.Tests
+{
+    [TestFixture]
+    public class EvenFibonacciNumbersTest
+    {
+
+        private EvenFibonacciNumbers _evenFibonacciNumbers;
+        private int Answer;
+
+
+        [SetUp]
+        protected void SetUp()
+        {
+            _evenFibonacciNumbers = new EvenFibonacciNumbers();
+        }
+
+        
+        [Test]
+        public void SumOfFibNumbersUnder10is19()
+        {
+            // 1+2+3+5+8 = 19
+
+            
+            for (int i = 1; i <= 3; i++)
+            {
+                _evenFibonacciNumbers.Sum += _evenFibonacciNumbers.currentFibonacciNumber;
+                _evenFibonacciNumbers.GetNextFibonacci();
+
+            }
+
+            Assert.AreEqual(19,_evenFibonacciNumbers.Sum);
+        }
+
+
+        [Test]
+        public void ShouldBePositiveSumForFibNumbersUnder100()
+        {
+            _evenFibonacciNumbers.Reset();
+
+
+            while (_evenFibonacciNumbers.currentFibonacciNumber < 100)
+            {
+                _evenFibonacciNumbers.Sum += _evenFibonacciNumbers.currentFibonacciNumber;
+                _evenFibonacciNumbers.GetNextFibonacci();
+                Console.WriteLine(_evenFibonacciNumbers.currentFibonacciNumber);
+            }
+
+            Console.WriteLine(_evenFibonacciNumbers.Sum);
+            Assert.IsTrue(_evenFibonacciNumbers.Sum > 0);
+        }
+
+        [Test]
+        public void SumOfFOnlyEvenFibNumbersUnder10is10()
+        {
+            // 2+8 = 10
+
+            _evenFibonacciNumbers.Reset();
+            _evenFibonacciNumbers.Sum = 2; //
+
+            for (int i = 1; i <= 3; i++)
+            {
+                if(_evenFibonacciNumbers.currentFibonacciNumber % 2==0)
+                    _evenFibonacciNumbers.Sum += _evenFibonacciNumbers.currentFibonacciNumber;
+                _evenFibonacciNumbers.GetNextFibonacci();
+
+            }
+
+            Assert.AreEqual(10, _evenFibonacciNumbers.Sum);
+        }
+
+        [Test]
+        public void SumOfFOnlyEvenFibNumbersNotExeceeding4millionisPositive()
+        {
+            // 2+8 = 10
+
+            _evenFibonacciNumbers.Reset();
+            _evenFibonacciNumbers.Sum = 2; //
+
+            while (_evenFibonacciNumbers.currentFibonacciNumber <= 4000000)
+            {
+                if (_evenFibonacciNumbers.currentFibonacciNumber % 2 == 0)
+                    _evenFibonacciNumbers.Sum += _evenFibonacciNumbers.currentFibonacciNumber;
+                Console.WriteLine(_evenFibonacciNumbers.currentFibonacciNumber);
+                _evenFibonacciNumbers.GetNextFibonacci();
+            }
+
+            Console.WriteLine(_evenFibonacciNumbers.Sum);
+
+            Assert.IsTrue(_evenFibonacciNumbers.Sum > 0);
+        }
+
+
+    }
+}
---
+++ ProjectEuler.Solutions/EvenFibonacciNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Solutions
+{
+    public class EvenFibonacciNumbers
+    {
+        public const int startingNumber1 = 1;
+        public const int startingNumber2 = 2;
+        private int number1 = startingNumber1;
+        private int number2 = startingNumber2;
+
+        public int currentFibonacciNumber = startingNumber1 + startingNumber2;
+        public int Sum = startingNumber1 + startingNumber2;
+
+
+        public void Reset()
+        {
+            currentFibonacciNumber = startingNumber1 + startingNumber2;
+            Sum = startingNumber1 + startingNumber2;
+        }
+
+        public void GetNextFibonacci()
+        {
+            number1 = number2;
+            number2 = currentFibonacciNumber;
+            currentFibonacciNumber = number1 + number2;
+        }
+
+        public void GetFibNumbers()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                GetNextFibonacci();
+            }
+        }
+    }
+}
