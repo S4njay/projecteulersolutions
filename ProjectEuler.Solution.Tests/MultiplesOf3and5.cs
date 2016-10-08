7cd4a90e66529e76905e3449533483af8db3b982
sanjay <sanjay.au@gmail.com>
2016-10-09 01:58:58 +1100

    Problem 1 Multiples of 3 and 5

---
+++ ProjectEuler.Solution.Tests/MultiplesOf3and5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ProjectEuler.Solutions;
+
+namespace ProjectEuler.Solution.Tests
+{
+    [TestFixture]
+    public class MultiplesOf3and5Test
+    {
+
+        private SumMultiplesOf3and5 _sumMultiplesOf3And5;
+        private int Answer;
+
+
+        [SetUp]
+        protected void SetUp()
+        {
+            _sumMultiplesOf3And5 = new SumMultiplesOf3and5();
+            _sumMultiplesOf3And5.numbersTo = 10;
+            Answer = _sumMultiplesOf3And5.GetAnswer();
+        }
+
+        [Test]
+        public void MustReturn23forNumbersTo10()
+        {
+            Assert.AreEqual(Answer,23);
+        }
+
+        [Test]
+        public void MustReturnPositiveResultForNumbersTo1000()
+        {
+            _sumMultiplesOf3And5.numbersTo = 1000;
+            Answer = _sumMultiplesOf3And5.GetAnswer();
+            Assert.IsTrue(Answer > 0);
+            Console.Write(Answer);
+        }
+
+
+    }
+}
---
+++ ProjectEuler.Solutions/SumMultiplesOf3and5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Solutions
+{
+    public class SumMultiplesOf3and5
+    {
+        private int numbersFrom {
+            get { return 1; }
+            set { }
+        }
+        public int numbersTo { get; set; }
+
+        public int GetAnswer()
+        {
+            var sum = 0;
+            for (var i = numbersFrom; i < numbersTo; i++)
+            {
+                if (i%3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
